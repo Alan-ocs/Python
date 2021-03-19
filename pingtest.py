453b1dc62e559e68c55e53fb6349a79dcbbd3d32
Alan Silva <80364531+Alan-ocs@users.noreply.github.com>
2021-03-19 16:27:59 -0300

    Now this can verify the logged user

--- pingtest.py
+++ pingtest.py
@@ -2,15 +2,20 @@ import os
 import csv
 import subprocess
 import socket
+from datetime import datetime
 
 name = {}
 CI = {}
 hostname = {}
 status = {}
+username = {}
+today = {}
+
 with open('Output1.csv', 'r', newline='') as csvinput:
     reader = csv.DictReader(csvinput)
 
     for rows in reader:
+        today = datetime.now()
         CI = rows['CI_Name']
         try:
             ip = socket.gethostbyname(CI)
@@ -25,12 +30,19 @@ with open('Output1.csv', 'r', newline='') as csvinput:
         print(response)
         if 'bytes=32' in response:
             status = 'Up'
+            username = subprocess.Popen(['powershell.exe','Get-WmiObject -class Win32_ComputerSystem -computername {} | Format-List Username'.format(hostname)], stdout = subprocess.PIPE).communicate()[0]
+            username = username.decode().strip()
+            if 'Get-WmiObject : The RPC server is unavailable.' in username:
+                username = 'unavailable'
         elif 'destination host unreachable' in response:
             status = 'Unreachable'
+            username = 'User Not Found'
         else:
             status = 'Down'
+            username = 'User Not Found'
         if status == 'Down':
             ip = 'Not Found'
+            username = 'User Not Found'
         with open('Output Final.csv', 'a', newline='') as csvoutput:
             output = csv.writer(csvoutput)
-            output.writerow([hostname] + [data] + [status] + [ip])
+            output.writerow([hostname] + [status] + [ip] + [username] + [today])
