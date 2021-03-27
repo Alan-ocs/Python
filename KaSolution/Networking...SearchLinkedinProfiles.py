e70c34e277b23d5347405dcb735dfdc5e3d72aa5
Alan Silva <80364531+Alan-ocs@users.noreply.github.com>
2021-03-27 11:52:38 -0300

    update

--- KaSolution/Networking...SearchLinkedinProfiles.py
+++ KaSolution/Networking...SearchLinkedinProfiles.py
@@ -1,10 +1,9 @@
 import re
 import webbrowser
 
-shakes = open("KaSolutionChat.txt", "r")
+linkedin = open("KaSolutionChat.txt", "r")
 
-for line in shakes:
-    if re.match("(.*)linkedin(.*)*", line):
-        webbrowser.open(line, new = 2)
-        print(line)
-
+for contatos in linkedin:
+    if re.match("(.*)linkedin(.*)*", contatos):
+        webbrowser.open(contatos, new = 2)
+        print(contatos)
